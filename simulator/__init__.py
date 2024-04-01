4994b4fbdc501e53d77675af3facb419069d2564
zzz <zzz510711928@gmail.com>
2024-04-01 16:43:15 +0800

    add comments

--- simulator/__init__.py
+++ simulator/__init__.py
@@ -2,6 +2,7 @@ from flgo.simulator.base import BasicSimulator
 
 class StaticUniSimulator(BasicSimulator):
     def initialize(self):
+        # Uniform(600s, 10800s) is equal to Uniform(10min, 3h)
         self.client_time_response = {cid: self.random_module.randint(600, 10800) for cid in self.clients}
         self.set_variable(list(self.clients.keys()), 'latency', list(self.client_time_response.values()))
 
@@ -12,6 +13,7 @@ class StaticUniSimulator(BasicSimulator):
 
 class DynamicUniSimulator(BasicSimulator):
     def initialize(self):
+        # Uniform(600s, 10800s) is equal to Uniform(10min, 3h)
         client_time_response = {cid: self.random_module.randint(600, 10800) for cid in self.clients}
         self.set_variable(list(self.clients.keys()), 'latency', list(client_time_response.values()))
 
