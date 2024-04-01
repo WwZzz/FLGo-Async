127ccf9ae6a93651b7b5834bf4989cc54d1fc080
zzz <zzz510711928@gmail.com>
2024-04-01 10:53:35 +0800

    add simulators

---
+++ simulator/__init__.py
@@ -0,0 +1,20 @@
+from flgo.simulator.base import BasicSimulator
+
+class StaticUniSimulator(BasicSimulator):
+    def initialize(self):
+        self.client_time_response = {cid: self.random_module.randint(5, 1000) for cid in self.clients}
+        self.set_variable(list(self.clients.keys()), 'latency', list(self.client_time_response.values()))
+
+    def update_client_responsiveness(self, client_ids):
+        latency = [self.client_time_response[cid] for cid in client_ids]
+        self.set_variable(client_ids, 'latency', latency)
+
+
+class DynamicUniSimulator(BasicSimulator):
+    def initialize(self):
+        client_time_response = {cid: self.random_module.randint(5, 1000) for cid in self.clients}
+        self.set_variable(list(self.clients.keys()), 'latency', list(client_time_response.values()))
+
+    def update_client_responsiveness(self, client_ids):
+        client_time_response = {cid: self.random_module.randint(5, 1000) for cid in client_ids}
+        self.set_variable(client_ids, 'latency', list(client_time_response.values()))
