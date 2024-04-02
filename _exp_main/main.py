f5b2e7e4299af698b15254686981ab6820015716
zzz <zzz510711928@gmail.com>
2024-04-02 14:44:47 +0800

    change parameters

--- _exp_main/main.py
+++ _exp_main/main.py
@@ -17,7 +17,8 @@ args_parser.add_argument('--gpu', type=int, default=0)
 args = args_parser.parse_args()
 
 task_model = {
-    'CIFAR100': model.ResNet18_CIFAR100
+    'CIFAR100': model.ResNet18_CIFAR100,
+    'CIFAR10': model.ResNet18_CIFAR10,
 }
 try:
     method = importlib.import_module('.'.join(['algorithm',args.method, ]))
@@ -36,7 +37,7 @@ else:
 
 task = f'../task/{args.task}'
 Simulator = None if args.simulator=='' else eval('.'.join(['simulator', args.simulator]))
-runner = flgo.init(task, method, {'gpu':args.gpu, 'num_epochs':5, 'learning_rate':0.01, 'sample':'uniform_available', 'proportion':1.0}, model=model, Simulator=Simulator)
+runner = flgo.init(task, method, {'gpu':args.gpu, 'num_epochs':1, 'learning_rate':0.01, 'sample':'uniform_available', 'proportion':1.0, 'lr_scheduler':0, 'learning_rate_decay':0.998}, model=model, Simulator=Simulator)
 
 runner.register_exit_condition(lambda server: server.gv.clock.current_time>args.max_time)
 runner.run()
